be70859ad313c2a1c7fd7b408446203db0852a79
Ji-Rath <garethaguilar@gmail.com>
2022-09-14 10:43:32 -0500

    Add CubeDamageCalculation to adjust damage dealt based on Armor and blocking

---
+++ Source/CubeAnimal/CubeDamageCalculation.cpp
@@ -0,0 +1,84 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CubeDamageCalculation.h"
+#include "AttributeSetBase.h"
+#include "Kismet/KismetMathLibrary.h"
+
+struct RPGDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Damage);
+
+	RPGDamageStatics()
+	{
+		// Capture the Target's DefensePower attribute. Do not snapshot it, because we want to use the health value at the moment we apply the execution.
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAttributeSetBase, Armor, Target, false);
+
+		// Also capture the source's raw Damage, which is normally passed in directly via the execution
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAttributeSetBase, Damage, Source, true);
+	}
+};
+
+static const RPGDamageStatics& DamageStatics()
+{
+	static RPGDamageStatics DmgStatics;
+	return DmgStatics;
+}
+
+UCubeDamageCalculation::UCubeDamageCalculation()
+{
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(DamageStatics().DamageDef);
+}
+
+void UCubeDamageCalculation::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+                                                    FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	UAbilitySystemComponent* TargetAbilitySystemComponent = ExecutionParams.GetTargetAbilitySystemComponent();
+	UAbilitySystemComponent* SourceAbilitySystemComponent = ExecutionParams.GetSourceAbilitySystemComponent();
+
+	AActor* SourceActor = SourceAbilitySystemComponent ? SourceAbilitySystemComponent->GetAvatarActor() : nullptr;
+	AActor* TargetActor = TargetAbilitySystemComponent ? TargetAbilitySystemComponent->GetAvatarActor() : nullptr;
+
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+
+	// Gather the tags from the source and target as that can affect which buffs should be used
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	// --------------------------------------
+	//	Damage Done = Damage * AttackPower / DefensePower
+	//	If DefensePower is 0, it is treated as 1.0
+	// --------------------------------------
+
+	float DefensePower = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluationParameters, DefensePower);
+	if (DefensePower == 0.0f)
+	{
+		DefensePower = 1.0f;
+	}
+
+	float Damage = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().DamageDef, EvaluationParameters, Damage);
+
+	// Blocking attacks logic
+	const FGameplayTag BlockTag = FGameplayTag::RequestGameplayTag(FName(TEXT("Attack.Block")));
+	
+	FVector Dir = UKismetMathLibrary::FindLookAtRotation(TargetActor->GetActorLocation(), SourceActor->GetActorLocation()).Vector().GetSafeNormal();
+	float Dot = Dir.Dot(TargetActor->GetActorForwardVector());
+	
+	if (Dot > 0.5 && TargetAbilitySystemComponent->HasMatchingGameplayTag(BlockTag))
+		DefensePower = Damage;
+
+	// Calculate damage based on defence and damage
+	const float DamageDone = Damage / DefensePower;
+	if (DamageDone > 0.f)
+	{
+		OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(DamageStatics().DamageProperty, EGameplayModOp::Additive, DamageDone));
+	}
+}
---
+++ Source/CubeAnimal/CubeDamageCalculation.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "CubeDamageCalculation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBEANIMAL_API UCubeDamageCalculation : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+
+	UCubeDamageCalculation();
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+};
