0db9c1c37666557301601db06d920ab7769df186
Gareth Aguilar <garethaguilar@gmail.com>
2022-11-12 21:02:50 -0600

    Add AnimURO component

---
+++ Source/CubeAnimal/AnimURO.cpp
@@ -0,0 +1,44 @@
+﻿// Copyright Wicked Games AG. All Rights Reserved.
+
+
+#include "AnimURO.h"
+
+
+UAnimURO::UAnimURO()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+	VisibleDistanceFactorThesholds.Add(0.24f);
+	VisibleDistanceFactorThesholds.Add(0.12f);
+}
+
+void UAnimURO::BeginPlay()
+{
+	Super::BeginPlay();
+	if (LODFrameRate.Num() <= 0) return;
+
+	UActorComponent* pCpt = GetOwner()->GetComponentByClass(USkinnedMeshComponent::StaticClass());
+	if (pCpt != nullptr)
+	{
+		USkinnedMeshComponent* pSKMesh = Cast<USkinnedMeshComponent>(pCpt);
+		pSKMesh->bEnableUpdateRateOptimizations = true;
+		pSKMesh->AnimUpdateRateParams->bShouldUseLodMap = bUseLOD;
+
+		if (bUseLOD)
+		{
+			for (int i = 0; i < LODFrameRate.Num(); i++)
+			{
+				pSKMesh->AnimUpdateRateParams->LODToFrameSkipMap.Add(i, LODFrameRate[i]);
+			}
+		}
+		else
+		{
+			pSKMesh->AnimUpdateRateParams->BaseVisibleDistanceFactorThesholds.Empty();
+			for (int i = 0; i < VisibleDistanceFactorThesholds.Num(); i++)
+			{
+				pSKMesh->AnimUpdateRateParams->BaseVisibleDistanceFactorThesholds.Add(VisibleDistanceFactorThesholds[i]);
+			}
+		}
+	}
+}
+
---
+++ Source/CubeAnimal/AnimURO.h
@@ -0,0 +1,32 @@
+﻿// Copyright Wicked Games AG. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AnimURO.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class CUBEANIMAL_API UAnimURO : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UAnimURO();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = URO, meta = (AllowPrivateAccess = "true"))
+	bool bUseLOD = false;
+	
+	UPROPERTY(EditAnywhere, Category = URO, meta = (AllowPrivateAccess = "true"))
+	TArray<int> LODFrameRate;
+
+	UPROPERTY(EditAnywhere, Category = URO, meta = (AllowPrivateAccess = "true"))
+	TArray<float> VisibleDistanceFactorThesholds;
+};
